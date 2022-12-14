512b4198dc02dde6b0ac9ce6f7f610b1e642892a
Sonik3311 <darkissoul2017@gmail.com>
2022-12-14 10:09:14 +0300

    test

--- setup.py
+++ setup.py
@@ -1,18 +1,9 @@
 import sys
 import subprocess
 
-
-packages = ['glm', 'moderngl', 'numpy', 'icosphere']
-# implement pip as a subprocess:
-
+packages = ['pyglm', 'moderngl', 'numpy', 'icosphere']
 
 for package in packages:
     subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
-
-
-# process output with an API in the subprocess module:
-#reqs = subprocess.check_output([sys.executable, '-m', 'pip',
-#'freeze'])
-#installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
 
 print("Done!")
