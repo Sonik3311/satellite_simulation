1b85c9537389133847d37b7a87a217c65ace7d14
Sonik3311 <darkissoul2017@gmail.com>
2022-12-14 17:08:22 +0300

    Updatet settigns

--- simulationsettings.py
+++ simulationsettings.py
@@ -1,25 +1,23 @@
 import numpy as np
 
-
-
-G_FACTOR = 11 # 6... * 10 ** -G_FACTOR      #ISS Data, use with ODE
-M = 1000000 #Planet Mass                    #5.972 * 10 ** 24
-R = 3 #Planet Radius                        #6371000
-r = 1 #Satellite height                     #473000
-
-SCALE = 10**0 #How much to scale system down (i plot everything in range less than 10m, but calculating orbits in thousands of km)
-VELOCITY = 6000 # speed, uncomment velocity below to use with PHYS
-POSITION = np.array([4,0,0]) #used for PHYS, position of the sattelite (sphere visualizer radius is 3 units)
-
-SOLVER = 'PHYS' 
+SOLVER = 'ODE'
 #ODE : good big orbits, no info (используйтя для +- точного соотношения орбиты и земли)
 #PHYS: VERY SLOW with big orbits, a lot of info (need to scale down simulation size like 10000km orbit radius -> 10m orbit radius so there are not a lot of calculations)
 
+G_FACTOR = 11
+if SOLVER == "ODE":
+    M = 5.972 * 10 ** 24 # Масса земли
+    R = 6371000 # Радиус земли
+    r = 473000 #Высота спутника
+    SCALE = 10**6 #Насколько уменьшить размер симуляции (я всё рисую в 10 метрах, когда орбиты могуть быть размером в тысячи км)
+    VELOCITY = 6000 #Скорость спутника
+else:
+    M = 1000000
+    R = 3 
+    r = 1 #трогайте
+    SCALE = 1 
+    VELOCITY = np.array([0,0.05,0.02], dtype=np.float32) #трогайте
+    POSITION = np.array([4,0,0]) #Позиция спутника, трогайте
 
 
-#///OPTIONS FOR PHYS SOLVER///#
-# UNCOMMENT WHAT IS COMMENTED WITH """ AND CHANGE ONLY WHEN PHYS SOLVER IS SELECTED
-VELOCITY = np.array([0,0.05,0.02], dtype=np.float32)
 
-#recomendations: PHYS - R = 3, r >= 1, M +- = 100000, поиграйтесь с массой, сложно угадать нужные значения
-#                ODE : Leave as is
