0d3763d397bf66eeb099dd4fd8c05db17c2d9c5d
Sonik3311 <darkissoul2017@gmail.com>
2022-12-14 16:47:19 +0300

    fix#1
    missing pygame module

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import sys
 import subprocess
 
-packages = ['pyglm', 'moderngl', 'numpy', 'icosphere']
+packages = ['pyglm', 'moderngl', 'numpy', 'icosphere', 'pygame']
 
 for package in packages:
     subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
