a5ee252acceff9abb9bec558d519cadcf71787d1
Sonik3311 <darkissoul2017@gmail.com>
2022-12-14 17:14:00 +0300

    Misc

--- simulationsettings.py
+++ simulationsettings.py
@@ -1,17 +1,20 @@
 import numpy as np
 
-SOLVER = 'ODE'
+SOLVER = 'PHYS'
 #ODE : good big orbits, no info (используйтя для +- точного соотношения орбиты и земли)
 #PHYS: VERY SLOW with big orbits, a lot of info (need to scale down simulation size like 10000km orbit radius -> 10m orbit radius so there are not a lot of calculations)
 
-G_FACTOR = 11
+G_FACTOR = 11 #не трогать
 if SOLVER == "ODE":
+    #Change if your solver is ODE
     M = 5.972 * 10 ** 24 # Масса земли
     R = 6371000 # Радиус земли
     r = 473000 #Высота спутника
     SCALE = 10**6 #Насколько уменьшить размер симуляции (я всё рисую в 10 метрах, когда орбиты могуть быть размером в тысячи км)
     VELOCITY = 6000 #Скорость спутника
+    POSITION = 1
 else:
+    #Change if your solver is PHYS
     M = 1000000
     R = 3 
     r = 1 #трогайте
