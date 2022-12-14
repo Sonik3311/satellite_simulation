09aea01d8b4c2edace9d871600394dc91d9591c7
Sonik3311 <darkissoul2017@gmail.com>
2022-12-14 16:57:24 +0300

    fix#2
    missing matplotlib

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 import sys
 import subprocess
 
-packages = ['pyglm', 'moderngl', 'numpy', 'icosphere', 'pygame']
+packages = ['pyglm', 'moderngl', 'numpy', 'icosphere', 'matplotlib']
+spackages = ['python']
 
 for package in packages:
     subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
+
+for package in spackages:
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', package, '--pre'])
 
 print("Done!")
